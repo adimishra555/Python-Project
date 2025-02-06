d244976ceccfbe53d372ea90917553f947c93ff0
adimishra555 <141378821+adimishra555@users.noreply.github.com>
2025-02-06 07:03:46 +0530

    Defang IP address.py

--- Defang IP address.py
+++ Defang IP address.py
@@ -6,3 +6,19 @@ def ip_address(address):
     return new_address
 ipaddress=ip_address("1.1.2.3")
 print(ipaddress)
+
+
+#------------------------------ To Find public IP address
+import requests
+
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        ip_data = response.json()
+        return ip_data['ip']
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    public_ip = get_public_ip()
+    print(f"Your public IP address is: {public_ip}")
